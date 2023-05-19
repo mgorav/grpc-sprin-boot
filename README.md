c57a662490e30ed53712a68980bf0bec92f58a61
gaurav <mgorav@gmail.com>
2023-05-19 18:39:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Spring Boot gRPC 
+Spring Boot gRPC
+
+## Build
+```
+mvn clean install 
+
+Run Spring boot app: GrpcSpringBootExampleApplication
+
+```
+
+```
+  grpcurl --plaintext localhost:9090 list
+  grpcurl --plaintext localhost:9090 list com.gonnect.grpc.GreetingService
+  grpcurl --plaintext -d '{"message": "How are you?"}' localhost:9090 com.gonnect.grpc.GreetingService/greeting
+  
+```
+
+#
---
+++ greeting-service/src/main/java/com/gonnect/grpc/service/GreetingServiceImpl.java
@@ -0,0 +1,26 @@
+package com.gonnect.grpc.service;
+
+import com.gonnect.grpc.*;
+import com.techprimers.grpc.GreetingRequest;
+import com.techprimers.grpc.GreetingResponse;
+import com.techprimers.grpc.GreetingServiceGrpc;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void greeting(GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
+        String message = request.getMessage();
+        System.out.println("Received Message: " + message);
+
+
+        GreetingResponse greetingResponse = GreetingResponse.newBuilder()
+                .setMessage("Received your " + message + ". Hello From Server. ")
+                .build();
+
+        responseObserver.onNext(greetingResponse);
+        responseObserver.onCompleted();
+    }
+}
